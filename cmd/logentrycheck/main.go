b121ead999f2035eacbd117dd5ae16dbcb427bc7
Ilya <ilya20051212@gmail.com>
2026-03-11 16:59:31 +0300

    feat: add basic analyzer

--- cmd/logentrycheck/main.go
+++ cmd/logentrycheck/main.go
@@ -1,4 +1,11 @@
 package main
 
+import (
+	"golang.org/x/tools/go/analysis/singlechecker"
+
+	"github.com/IlyaChern12/logentrycheck/internal/analyzer"
+)
+
 func main() {
+	singlechecker.Main(analyzer.Analyzer)
 }
---
+++ internal/analyzer/rules/rules.go
@@ -0,0 +1,6 @@
+package rules
+
+import "golang.org/x/tools/go/analysis"
+
+// All contains registered rules.
+var All = []*analysis.Analyzer{}
