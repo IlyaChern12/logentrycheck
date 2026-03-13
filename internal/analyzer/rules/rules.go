5eb01b6039355b6704a9474377f862b099a94395
Ilya <ilya20051212@gmail.com>
2026-03-13 15:43:47 +0300

    feat: add configuration for rules

--- internal/analyzer/rules/rules.go
+++ internal/analyzer/rules/rules.go
@@ -9,3 +9,26 @@ var All = []*analysis.Analyzer{
 	SpecialCharsAnalyzer,
 	SensitiveAnalyzer,
 }
+
+// Active returns the list of enabled analyzers based on disable flags.
+func Active(disableLowercase, disableEnglish, disableSpecialChars, disableSensitive bool) []*analysis.Analyzer {
+	var active []*analysis.Analyzer
+
+	if !disableLowercase {
+		active = append(active, LowercaseAnalyzer)
+	}
+
+	if !disableEnglish {
+		active = append(active, EnglishAnalyzer)
+	}
+
+	if !disableSpecialChars {
+		active = append(active, SpecialCharsAnalyzer)
+	}
+
+	if !disableSensitive {
+		active = append(active, SensitiveAnalyzer)
+	}
+
+	return active
+}
